5424d27ca0f22b25806038b6cc08052af3fc14d5
korelin_ks <johnny_85@list.ru>
2026-06-25 11:36:01 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+import { WebSocketServer } from "ws";
+import WebSocket from "ws";
+
+const wss = new WebSocketServer({port: 8080});
+
+const symbols = [
+    "btcusdt",
+    "ethusdt",
+    "ltcusdt",
+    "bnbusdt",
+    "xrpusdt",
+    "adausdt",
+    "dogeusdt",
+    "solusdt",
+];
+
+const streams = symbols.map(s =>`${s}@ticker`).join('/')
+const binanceUrl = `wss://stream.binance.com:9443/stream?streams=${streams}`;
+
+const binanceSocket = new WebSocket(binanceUrl);
+
+binanceSocket.onmessage = (event) => {
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(event.data);
+        }
+    });
+};
+
+binanceSocket.onclose = () => {
+    console.log("Соединение с биржей закрыто");
+};
+console.log("WebSocket сервер запущен на порту 8080");
+
+wss.on("connection", () => {
+    console.log("новый клиент подключился");
+
+    ws.on("close", () => {
+        console.log("клиент отключился");
+    });
+});
